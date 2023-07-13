c5814040fc956c84566a54a48fa23c744fd125d9
Santhosh-004 <santhoshg0004@gmail.com>
2023-07-13 16:46:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -282,24 +282,18 @@ var winner = (board) => {
 var check_win = (board) => {
     for (let i = 0; i < 3; i++) {
         if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== '') {
-            console.log('here')
             return board[i][0]
         }
     }
     for (let i = 0; i < 3; i++) {
         if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== '') {
-            console.log('here2')
             return board[0][i]
         }
     }
     if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== '') {
-        console.log('here3')
         return board[0][0]
     }
     if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== '') {
-        console.log('here4')
-        console.log(board[0][0])
-        console.log('see here', board[0][2], board[1][1], board[2][0])
         return board[0][2]
     }
     return false
